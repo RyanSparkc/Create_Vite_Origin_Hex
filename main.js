69aa4c600d4593c3dbad26cc411cc0d3ef6b59bd
forest079 <bluegx08@gmail.com>
2023-12-07 11:44:06 +0800

    [add]: 更新 JS 版本檔案

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 import './assets/scss/all.scss';
+import Swal from 'sweetalert2';
 
+// menu 切換
+let menuOpenBtn = document.querySelector('.menuToggle');
+let linkBtn = document.querySelectorAll('.topBar-menu a');
+let menu = document.querySelector('.topBar-menu');
+menuOpenBtn.addEventListener('click', menuToggle);
 
-import $ from 'jquery';
-window.jQuery = window.$ = $;
-console.log($);
+linkBtn.forEach((item) => {
+  item.addEventListener('click', closeMenu);
+})
 
-import './assets/js/all.js';
+function menuToggle() {
+  if (menu.classList.contains('openMenu')) {
+    menu.classList.remove('openMenu');
+  } else {
+    menu.classList.add('openMenu');
+  }
+}
+function closeMenu() {
+  menu.classList.remove('openMenu');
+}
 
-console.log("main.js 入口檔案");
+// 錯誤處理提示
+export function sweetalert2(text) {
+  Swal.fire({
+    position: "top-end",
+    text: text,
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+  });
+}
+
+// 金錢千分位處理
+export function formattedNumber(price) {
+  return price.toLocaleString('zh-TW');
+}
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 import { glob } from 'glob';
 
-import inject from '@rollup/plugin-inject';
-
 import liveReload from 'vite-plugin-live-reload';
 
 function moveOutputPlugin() {
@@ -27,21 +25,12 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  // base: '/web-layout-training-vite/',
+  base: '/web-layout-training-vite/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
     moveOutputPlugin(),
-    inject({
-      $: 'jquery', // 这里会自动载入 node_modules 中的 jquery
-      jQuery: 'jquery',
-      'windows.jQuery': 'jquery',
-    }),
   ],
-  css: {
-    // 增加 source map
-    devSourcemap: true,
-  },
   server: {
     // 啟動 server 時預設開啟的頁面
     open: 'pages/index.html',
@@ -52,10 +41,7 @@ export default defineConfig({
         glob
           .sync('pages/**/*.html')
           .map((file) => [
-            path.relative(
-              'pages',
-              file.slice(0, file.length - path.extname(file).length)
-            ),
+            path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
